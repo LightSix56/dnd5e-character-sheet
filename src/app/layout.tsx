54151302a1f8ebde6f0ed607fa832159cfea2475
LightSix56 <105928444+LightSix56@users.noreply.github.com>
2026-08-10 02:54:02 +0300

    Update layout.tsx
    Add google metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,17 +14,27 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Генератор листа персонажа D&D 5e",
-  description: "Интерактивный генератор листа персонажа D&D 5e с авторасчётом характеристик и экспортом в DOCX",
-  keywords: ["D&D", "DnD", "5e", "character sheet", "лист персонажа", "генератор", "DOCX"],
+  description:
+    "Интерактивный генератор листа персонажа D&D 5e с авторасчётом характеристик и экспортом в DOCX",
+  keywords: [
+    "D&D",
+    "DnD",
+    "5e",
+    "character sheet",
+    "лист персонажа",
+    "генератор",
+    "DOCX",
+  ],
+  verification: {
+    google: "sWRTxBT4WwuvIpxVMuBFPGGADfxThFQ1x1x-MkiclmE",
+  },
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: '32x32' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: "/favicon.ico", sizes: "32x32" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
-    apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180' },
-    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
   },
 };
 
